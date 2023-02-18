ef93c63341c119cb70cc7a0ab0c4be2a92315b6f
Alex Smith <109144005+AlexMSm@users.noreply.github.com>
2023-02-18 20:39:24 +0000

    mobile screen optimisations

--- portfolio-site/src/components/Navbar/index.tsx
+++ portfolio-site/src/components/Navbar/index.tsx
@@ -1,43 +1,46 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import './index.scss'
-import LogoS from '../../assets/images/A-logo-white.png'
-import LogoSubtitle from '../../assets/images/name-logo-white.png'
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./index.scss";
+import LogoS from "../../assets/images/A-logo-white.png";
+import LogoSubtitle from "../../assets/images/name-logo-white.png";
 import {
   faHome,
   faUser,
   faEnvelope,
   faSuitcase,
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
   let activeStyle = {
-    textDecoration: 'underline',
-    backgroundColor: '#003049',
-  }
-  let activeClassName = 'underline'
-  let isActive: boolean
+    textDecoration: "underline",
+    backgroundColor: "#003049",
+  };
+  let activeClassName = "underline";
+  let isActive: boolean;
 
   return (
     <div className="nav-bar">
-      <Link className="logo" to="/">
+      <Link className="logo" to="/" onClick={() => setShowNav(false)}>
         <img src={LogoS} alt="logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="sublogo" />
       </Link>
-      <nav>
+      <nav className={showNav ? "mobile-show" : ""}>
         <NavLink
           className="home-link"
           to="/"
           style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          onClick={() => setShowNav(false)}
         >
           <FontAwesomeIcon icon={faHome} color="#fdf0d5" />
         </NavLink>
-
         <NavLink
           className="details-link"
           to="/details"
           style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          onClick={() => setShowNav(false)}
         >
           <FontAwesomeIcon icon={faUser} color="#fdf0d5" />
         </NavLink>
@@ -46,6 +49,7 @@ const Navbar = () => {
           className="contact-link"
           to="/contact"
           style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          onClick={() => setShowNav(false)}
         >
           <FontAwesomeIcon icon={faEnvelope} color="#fdf0d5" />
         </NavLink>
@@ -53,12 +57,20 @@ const Navbar = () => {
           className="portfolio-link"
           to="/portfolio"
           style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          onClick={() => setShowNav(false)}
         >
           <FontAwesomeIcon icon={faSuitcase} color="#fdf0d5" />
         </NavLink>
       </nav>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- portfolio-site/src/components/Portfolio/ProjectCard/index.tsx
+++ portfolio-site/src/components/Portfolio/ProjectCard/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import { Card } from 'primereact/card'
-import { Button } from 'primereact/button'
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
 
 interface Props {
-  title: string
-  description: string
-  cover: string
-  hover: string
-  url: string
-  stack: string[]
+  title: string;
+  description: string;
+  cover: string;
+  hover: string;
+  url: string;
+  stack: string[];
 }
 
 const ProjectCard: React.FC<Props> = ({
@@ -20,29 +20,29 @@ const ProjectCard: React.FC<Props> = ({
   stack,
   url,
 }) => {
-  const header = <img alt="Card" src={hover} />
+  const header = <img alt="Card" src={hover} />;
   const footer = (
     <span>
       <Button label="Save" />
       <Button label="Cancel" />
     </span>
-  )
+  );
 
   return (
     <div>
       <Card
         title={title}
         subTitle={stack}
-        style={{ width: '25em' }}
+        style={{ width: "25em" }}
         footer={footer}
         header={header}
       >
-        <p className="m-0" style={{ lineHeight: '2.5' }}>
+        <p className="m-0" style={{ lineHeight: "2.5" }}>
           {description}
         </p>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
